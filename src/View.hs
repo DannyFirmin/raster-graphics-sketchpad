fce890343f18ae4c6c564fe7d6ba163bd6b2657f
Tony Hosking <antony.hosking@anu.edu.au>
2018-04-23 13:49:43 +1000

    Fixing doctests to compare ordered rasters.

--- src/View.hs
+++ src/View.hs
@@ -61,15 +61,17 @@ pointRaster p = [(p, 1)]
 
 -- | A raster for the rectangle with corner coordinates (x1,y1) and (x2,y2)
 -- Examples:
--- >>> rectangleRaster (-1,-1) (1,1)
--- [((-1,-1),1.0),((-1,1),1.0),((1,-1),1.0),((1,1),1.0),((0,-1),1.0),((0,1),1.0),((-1,0),1.0),((1,0),1.0)]
+-- >>> import Data.List(sort)
+-- >>> sort $ rectangleRaster (-1,-1) (1,1)
+-- [((-1,-1),1.0),((-1,0),1.0),((-1,1),1.0),((0,-1),1.0),((0,1),1.0),((1,-1),1.0),((1,0),1.0),((1,1),1.0)]
 rectangleRaster :: Coord -> Coord -> Raster
 rectangleRaster = undefined -- TODO
 
--- | A raster for the line with endpoints (x1,y1) and (x2,y2).
+-- | A raster for the line with end coordinates given as arguments.
 -- Antialias if smooth is true.
 -- Examples:
--- >>> lineRaster False (-1,-1) (1,1)
+-- >>> import Data.List(sort)
+-- >>> sort $ lineRaster False (-1,-1) (1,1)
 -- [((-1,-1),1.0),((0,0),1.0),((1,1),1.0)]
 --
 -- prop> a == (fst $ head $ lineRaster False a b)
@@ -80,7 +82,8 @@ lineRaster = undefined -- TODO
 -- | A raster for the polyline with vertices vs.
 -- Antialias if smooth is true.
 -- Examples:
--- >>> polyLineRaster False [(0,0),(2,2)]
+-- >>> import Data.List(sort)
+-- >>> sort $ polyLineRaster False [(0,0),(2,2)]
 -- [((0,0),1.0),((1,1),1.0),((2,2),1.0)]
 --
 -- prop> lineRaster False a b == polyLineRaster False [a, b]
@@ -90,7 +93,8 @@ polyLineRaster = undefined -- TODO
 -- | A raster for the circle with center (x1,y1) and intersecting (x2,y2)
 -- Antialias if smooth is true.
 -- Examples:
--- >>> circleRaster False (0,0) (1,1)
--- [((0,1),1.0),((0,1),1.0),((0,-1),1.0),((0,-1),1.0),((1,0),1.0),((-1,0),1.0),((1,0),1.0),((-1,0),1.0)]
+-- >>> import Data.List(sort)
+-- >>> sort $ circleRaster False (0,0) (1,1)
+-- [((-1,0),1.0),((-1,0),1.0),((0,-1),1.0),((0,-1),1.0),((0,1),1.0),((0,1),1.0),((1,0),1.0),((1,0),1.0)]
 circleRaster :: Smooth -> Coord -> Coord -> Raster
 circleRaster = undefined -- TODO
